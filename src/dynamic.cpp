385c7129ad41f62a834b6bc4faeb0d38583297ba
KoningWoefie <sander@hdms.nl>
2023-08-19 17:03:06 +0200

    adding dynamic

---
+++ src/dynamic.cpp
@@ -0,0 +1,43 @@
+#include <src/dynamic.h>
+
+Dynamic::Dynamic()
+{
+    this->position = glm::vec3(0.0f, 0.0f, 0.0f);
+    this->scale = glm::vec3(1.0f, 1.0f, 1.0f);
+    this->rotation = 0.0f;
+
+    this->parent = nullptr;
+}
+
+Dynamic::~Dynamic()
+{
+    for (Dynamic* child : this->_children)
+    {
+        delete child;
+        child = nullptr;
+    }
+}
+
+void Dynamic::AddChild(Dynamic* child)
+{
+    if(child->parent != nullptr)
+    {
+        child->parent->RemoveChild(child);
+    }
+    _children.push_back(child);
+    child->parent = this;
+}
+
+void Dynamic::RemoveChild(Dynamic* child)
+{
+    for (int i = 0; i < _children.size(); i++)
+    {
+        if(_children[i] != child)
+        {
+            continue;
+        }
+        _children.erase(_children.begin() + i);
+        return;
+    }
+    std::cout << "Child not found" << std::endl;
+}
---
+++ src/dynamic.h
@@ -0,0 +1,31 @@
+#ifndef DYNAMIC_H
+#define DYNAMIC_H
+
+#include <iostream>
+#include <glm/glm.hpp>
+#include <vector>
+
+class Dynamic
+{
+public:
+    Dynamic();
+    ~Dynamic();
+
+    virtual void update(float deltaTime) = 0;
+
+    void AddChild(Dynamic* child);
+    void RemoveChild(Dynamic* child);
+
+    std::vector<Dynamic*> GetChildren() { return _children; };
+
+    Dynamic* parent;
+
+    glm::vec3 position;
+    glm::vec3 scale;
+    float rotation;
+private:
+    std::vector<Dynamic*> _children;
+};
+
+
+#endif
