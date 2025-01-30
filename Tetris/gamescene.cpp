f50312fec9bb40e0979927274921b9e7b8212217
KoningWoefie <sander@hdms.nl>
2025-01-30 09:51:13 +0100

    Grid added
    Empty grid was added

--- Tetris/gamescene.cpp
+++ Tetris/gamescene.cpp
@@ -35,4 +35,34 @@ void GameScene::Initialize()
 
     _canvas->AddChild(_backgroundImage);
     this->AddChild(_canvas);
+
+    colNum = 10;
+    rowNum = 20;
+
+    CreateGrid();
+}
+
+void GameScene::CreateGrid()
+{
+    _gridObject = new EmptyObject();
+    Components::UIElement* uiElement = new Components::UIElement();
+    uiElement->SetAlignment(Components::UIAlignment::Top);
+    _gridObject->AddComponent(uiElement);
+    _gridObject->transform->position = glm::vec3(-36.0f, 44.0f, 0.0f);
+    _canvas->AddChild(_gridObject);
+
+    for(int x = 0; x < colNum; x++)
+    {
+        _grid.push_back(std::vector<EmptyObject*>());
+        for(int y = 0; y < rowNum; y++)
+        {
+            _grid[x].push_back(new EmptyObject());
+            Components::Image* image = new Components::Image();
+            image->AddSprite(new Sprite("assets/EmptyBlock.tga", 0));
+            _grid[x][y]->AddComponent(image);
+            _grid[x][y]->transform->scale = glm::vec3(0.25f, 0.25f, 1.0f);
+            _gridObject->AddChild(_grid[x][y]);
+            _grid[x][y]->transform->position = glm::vec3(8.0f * x, 8.0f * y, 0.0f);
+        }
+    }
 }
--- Tetris/gamescene.h
+++ Tetris/gamescene.h
@@ -4,6 +4,7 @@
 #include <src/Core/scene.h>
 #include <src/UI/canvas.h>
 #include <src/UI/image.h>
+#include <src/UI/uielement.h>
 #include <src/Objects/emptyobject.h>
 #include <src/Core/config.h>
 
@@ -19,6 +20,14 @@ public:
 private:
     EmptyObject* _canvas;
     EmptyObject* _backgroundImage;
+
+    void CreateGrid();
+
+    int colNum;
+    int rowNum;
+
+    std::vector<std::vector<EmptyObject*>> _grid;
+    EmptyObject* _gridObject;
 };
 
 #endif
